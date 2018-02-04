1801a33112dccb9d85a88d9ce07ab67541cde5c3
msharma <msharma@usgbc.org>
2018-02-03 23:02:09 -0500

    My commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,49 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AmChartsService, AmChart } from "@amcharts/amcharts3-angular";
 
 import { HomePage } from '../pages/home/home';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { LoginPage } from '../pages/login/login';
+import { AnalysisPage } from '../pages/analysis/analysis';
+import { AddProjectPage } from '../pages/add-project/add-project';
+import { ProjectListPage } from '../pages/project-list/project-list';
+import { ProjectDetailsPage } from '../pages/project-details/project-details';
+import { ProjectAnalysisPage } from '../pages/project-analysis/project-analysis';
+import { SourcePage } from '../pages/source/source';
+import { AddMeterDataPage } from '../pages/add-meter-data/add-meter-data';
+import { SignupPage } from '../pages/signup/signup';
+import { AddSourcePage } from '../pages/add-source/add-source';
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any = DashboardPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
+      statusBar.overlaysWebView(false);
       splashScreen.hide();
+      
+      //  if (platform.is('ios')) {
+      //     let 
+      //       appEl = <HTMLElement>(document.getElementsByTagName('ION-APP')[0]),
+      //       appElHeight = appEl.clientHeight;
+
+      //     //Keyboard.disableScroll(true);
+
+      //     window.addEventListener('native.keyboardshow', (e) => {
+      //       appEl.style.height = (appElHeight - (<any>e).keyboardHeight) + 'px';
+      //     });
+
+      //     window.addEventListener('native.keyboardhide', () => {
+      //       appEl.style.height = '100%';
+      //     });
+
+      //   }
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,94 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
+import { AmChartsModule } from "@amcharts/amcharts3-angular";
 
+//pages
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DashboardPage } from '../pages/dashboard/dashboard';
+import { LoginPage } from '../pages/login/login';
+import { AnalysisPage } from '../pages/analysis/analysis';
+import { RegisterationPage } from '../pages/registeration/registeration';
+import { AddProjectPage } from '../pages/add-project/add-project';
+import { ProjectListPage } from '../pages/project-list/project-list';
+import { ProjectDetailsPage } from '../pages/project-details/project-details';
+import { ProjectAnalysisPage } from '../pages/project-analysis/project-analysis';
+import { SourcePage } from '../pages/source/source';
+import { AddMeterDataPage } from '../pages/add-meter-data/add-meter-data';
+import { AddSourcePage } from '../pages/add-source/add-source';
+import { SignupPage } from '../pages/signup/signup';
+
+//Providers
+import { SharedServiceProvider } from '../providers/shared-service/shared-service';
+import { BuildingProjectServiceProvider } from '../providers/building-project-service/building-project-service';
+
+//Components
+import { FabIconComponent } from '../components/fab-icon/fab-icon';
+import { SearchServiceProvider } from '../providers/search-service/search-service';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DashboardPage,
+    LoginPage,
+    AnalysisPage,
+    RegisterationPage,
+    AddProjectPage,
+    ProjectListPage,
+    FabIconComponent,
+    ProjectDetailsPage,
+    ProjectAnalysisPage,
+    SourcePage,
+    AddMeterDataPage,
+    SignupPage,
+    AddSourcePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    AmChartsModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp, {
+      platforms: {
+        ios: {
+          statusbarPadding: true
+        }
+      }
+      }),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DashboardPage,
+    LoginPage,
+    AnalysisPage,
+    RegisterationPage,
+    AddProjectPage,
+    ProjectListPage,
+    FabIconComponent,
+    ProjectDetailsPage,
+    ProjectAnalysisPage,
+    SourcePage,
+    AddMeterDataPage,
+    SignupPage,
+    AddSourcePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    SharedServiceProvider,
+    BuildingProjectServiceProvider,
+    SearchServiceProvider,
   ]
 })
 export class AppModule {}
